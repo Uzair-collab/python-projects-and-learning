0219cddce917e92abfa39c88c8f780b40051f5c0
Uzair-collab <uzair.tawakkalbd@gmail.com>
2024-09-16 18:23:49 +0600

    first hw

---
+++ hw.py
@@ -0,0 +1,9 @@
+
+maths = 40 * (100/100)
+science = 70 * (100/100)
+hindi = 50 * (100/100)
+english = 60 * (100/100)
+print('Raj got in science', science,"%")
+print('Raj got in maths', maths,"%")
+print('Raj got in hindi', hindi,"%")
+print('Raj got in english', english,"%")
