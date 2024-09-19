973bebe0ca4863b3d963aa96214c27e2d53ce0b3
Uzair-collab <uzair.tawakkalbd@gmail.com>
2024-09-20 00:26:33 +0600

    even or odd

---
+++ even-odd.py
@@ -0,0 +1,6 @@
+numb = int(input("enter a digit to determine if it is odd or even : "))
+
+if (numb % 2 == 0):
+    print("no : is even")
+else:
+    print("no : is odd")    
---
+++ profit_calculator.py
@@ -0,0 +1,14 @@
+actual_sale = float (input ("enter the selling price:"))
+actual_cost = float (input(" enter the actual price:"))
+
+if (actual_sale > actual_cost):
+    profit = actual_sale - actual_cost
+    print("profit:", profit)
+else:
+    loss = actual_cost - actual_sale
+    print("loss:", loss)
+
+
+
+
+    
