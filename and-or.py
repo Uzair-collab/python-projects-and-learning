18542b0d8d6f9b76550eaa3ee3e80e94d07dd222
Uzair-collab <uzair.tawakkalbd@gmail.com>
2024-09-21 18:13:05 +0600

    and-or practice

---
+++ and-or.py
@@ -0,0 +1,10 @@
+a = (int(input("type a number:")))
+b = (int(input("type a number:")))
+c = (int(input("type a number:")))
+
+if (a > 0 and b > 0 and c > 0):
+    print("all digits are greater than Safwan's grades")
+elif (a > 0 or b > 0 or c > 0):
+    print("one or two of them is greater than Safwan's grades")
+else:
+    print("not all digits are greater than Safwan's grades")
