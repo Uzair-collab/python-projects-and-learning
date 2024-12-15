01523c3b4ecbc461a502ee871f71035cf125879c
Uzair-collab <uzair.tawakkalbd@gmail.com>
2024-12-15 18:51:11 +0600

    func

---
+++ function.py
@@ -0,0 +1,8 @@
+num11= (int(input("input your num1: ")))
+num12= (int(input("input your num1: ")))
+def my_func(num1, num2):
+    print ("total sum of num1 and num 2 is :", num1 + num2)
+
+my_func(num11, num12)
+
+my_func(10, 20)
