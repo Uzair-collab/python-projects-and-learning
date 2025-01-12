79b095010173f69dfb4f80faf9210a30a27f3576
Uzair-collab <uzair.tawakkalbd@gmail.com>
2025-01-12 19:15:10 +0600

    hw

---
+++ funhw.py
@@ -0,0 +1,14 @@
+numa= (int(input("input your num1: ")))
+numb= (int(input("input your num1: ")))
+def my_func(num1, num2):
+    print ("total sum of num1 and num 2 is :", num1 + num2)
+
+def my_func2(num1, num2):
+    print ("difference between num1 and num 2 is :", num1 - num2)
+
+def my_func3(num1, num2):
+    print ("product of num1 and num 2 is :", num1 * num2)
+
+my_func(numa, numb)
+my_func2(numa, numb)
+my_func3(numa, numb)
